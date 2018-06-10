084d1ac881294e64acd7396bad9ec607a826e61b
drakosha108 <drakosha108@gmail.com>
2018-06-10 16:15:04 +0300

    Скрипти для нових дій

--- scripts.js
+++ scripts.js
@@ -18,13 +18,21 @@ $('.slider-2-for').slick({
   slidesToScroll: 1,
   arrows: false,
   fade: true,
-  asNavFor: '.slider-2-nav'
+  asNavFor: '.slider-2-nav,.slider-2-text'
+});
+
+$('.slider-2-text').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-2-nav,.slider-2-for'
 });
 
 $('.slider-2-nav').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
-  asNavFor: '.slider-2-for',
+  asNavFor: '.slider-2-for,.slider-2-text',
   dots: true,
   centerMode: true,
   focusOnSelect: true,
@@ -44,9 +52,18 @@ $('#nav-home').click(e => {
 $('#nav-about').click(e => {
   $('body').scrollTo('#about', 500);
 });
+$('#nav-gallery').click(e => {
+  $('body').scrollTo('#gallery', 500);
+});
 $('#nav-products').click(e => {
   $('body').scrollTo('#products', 500);
 });
 $('#nav-contact').click(e => {
   $('body').scrollTo('#contact', 500);
+});
+$('.button-1').click(e => {
+  $('body').scrollTo('#contact', 500);
+});
+$('.button-2').click(e => {
+  $('body').scrollTo('#subscribe', 500);
 });
