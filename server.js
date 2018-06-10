08d0cbd14ea8eade3548ca9677c450e808d6e611
drakosha108 <drakosha108@gmail.com>
2018-06-10 18:33:06 +0300

    Перенесено статичний контент в /public

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const cors = require('cors');
 const port = parseInt(process.env.PORT, 10) || 8090;
 
 app.use(cors());
-app.use(express.static('.'));
+app.use(express.static('./public'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
